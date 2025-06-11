209552ac8f6e482100cc768fd071efcab1191169
QwertyMD <aaryandhakal2020@gmail.com>
2025-06-11 14:10:51 +0545

    Add Frontned directory

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/" element />
+          <Route path="/" element />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
