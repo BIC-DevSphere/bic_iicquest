68250826045cf469988466f0ac7c87a022efccfd
Shuvam BC <bcshuvam@gmail.com>
2025-06-11 17:44:08 +0545

    SB-01-Models

--- backend/src/models/commynityPost.js
+++ backend/src/models/commynityPost.js
@@ -1,6 +1,29 @@
 import mongoose from "mongoose";
 
-export const communityPostSchema = new mongoose.Schema({
+const CommentSchema = new mongoose.Schema({
+    body: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User',
+        required: true,
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'CommunityPost',
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+const communityPostSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
@@ -13,4 +36,30 @@ export const communityPostSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId, ref: 'User',
         required: true,
     },
+    comments: {
+        type: [CommentSchema],
+        required: false,
+    },
+    totalComments: {
+        type: Number,
+        default: 0,
+    },
+    likes: {
+        type: Number,
+        default: 0,
+    },
+    dislikes: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    }
 })
+
+export default mongoose.model('CommunityPost', communityPostSchema);
